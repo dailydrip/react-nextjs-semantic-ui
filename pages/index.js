9b407b21ff991f9eb2b179fc8e51cd1260d19445
Franzé Jr <franzejr@gmail.com>
2017-08-07 15:42:12 -0300

    Start the project - Setup NextJS + Two pages

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default () => (
+  <div>
+    <h2>Welcome to next.js!</h2>
+    <Link href="/post"><a>Go to Post</a></Link>
+  </div>
+);
---
+++ pages/post.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default () => (
+  <div>
+    <h2>Our Blog Post!</h2>
+    <Link href="/"><a>Go to Initial</a></Link>
+  </div>
+);
